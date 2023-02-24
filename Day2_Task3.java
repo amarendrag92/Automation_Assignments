1ad883d5fd3aa907c28a90bb649a2dfe0afcca1b
amarendrag92 <126228516+amarendrag92@users.noreply.github.com>
2023-02-24 12:16:28 +0530

    Add files via upload
    Day2_Task

---
+++ Day2_Task3.java
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Day2_Task3
+
+
+{
+	public static void main(String[] args) {
+	    
+	  int i =25;
+	  
+	 for (i =25; i>0; i-- ) {
+	 if (i%2!=0) {
+	  
+
+    	  
+	      
+	  
+	       	       System.out.println(i);
+	   } else {
+	      // System.out.println("failed");
+
+	   }	   
+}
+	}
+}	    
+		    
+	
