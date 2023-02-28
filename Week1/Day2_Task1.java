a6262d356c9d6b13fa317ab78866ab208e81cc8f
amarendrag92 <126228516+amarendrag92@users.noreply.github.com>
2023-02-28 06:25:05 +0530

    Add files via upload

---
+++ Week1/Day2_Task1.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Day2_Task1
+
+
+{
+	public static void main(String[] args) {
+	    
+	  int i =26;
+	  
+	  if (i%2==0)
+	  
+	  {
+	      
+	  
+	       	       System.out.println("prime");
+	   } else 
+	       System.out.println("odd");
+	   
+}
+	}
+	    
+	    
+	
---
+++ Week1/Day2_Task2.java
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Day2_Task2
+
+
+{
+	public static void main(String[] args) {
+	    
+	   int num=27;
+	   //for (int i=4; i=10; i++ ) 
+	   boolean val= false;
+	   
+	   for ( int i =0; i<num; i++) {
+	      if (num%2!=0){
+	        val= true;
+	        break;
+	      }
+	      
+	       
+	   } if (val!=true){
+	       System.out.println("prime");
+	   } else {
+	       System.out.println("odd");
+	   }
+	   }
+	   
+	}
+	    
+	    
+	
