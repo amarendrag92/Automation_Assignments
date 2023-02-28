35d32f56958ce1eda668ac1acbc6cbf2b065fcdf
amarendrag92 <126228516+amarendrag92@users.noreply.github.com>
2023-02-28 06:26:03 +0530

    Add files via upload

---
+++ week2_day1/SecondLargestNum.java
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class SecondLargestNum
+
+
+{
+	public static void main(String[] args) {
+	    
+	  int data[] ={2,5,3,1,4,5,7,6,3,2};
+	  
+	//array.sort(data);
+//	array.sort(data);
+	Arrays.sort(data);
+	  
+	  for ( int i=0; i<data.length; i++){
+	  
+	  System.out.println(data[data.length-3]);
+
+    	  
+	      
+	  
+	       	       
+	  
+
+}	    
+}		    
+}	
